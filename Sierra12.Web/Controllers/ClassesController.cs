45ed7e48107d9941539fb4fff08b120cc4746501
Jim Gary <jimgaryphoenix@gmail.com>
2014-05-13 14:13:26 -0700

    Classes details pages refactor

---
+++ Sierra12.Web/Controllers/ClassesController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using ProgressTen.Infrastructure.ViewModels;
+using ProgressTen.Services;
+
+namespace ProgressTen.Web.Controllers
+{
+	[HandleError]
+	public class ClassesController : BaseController
+	{
+		public ActionResult Index()
+		{
+			return View("Index");
+		}
+
+		public ActionResult CombatRifle()
+		{
+			return View("CombatRifle");
+		}
+
+		//public ActionResult About()
+		//{
+		//    return View("About");
+		//}
+
+		//public ActionResult Contact()
+		//{
+		//    return View("Contact");
+		//}
+	}
+}
